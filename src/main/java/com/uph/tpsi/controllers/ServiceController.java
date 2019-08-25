bd990771478c0390837028749636e8b9406398db
Grzesiek <grzesiek555_12@outlook.com>
2019-08-25 14:07:31 +0200

    Finish back end

--- src/main/java/com/uph/tpsi/controllers/ServiceController.java
+++ src/main/java/com/uph/tpsi/controllers/ServiceController.java
@@ -1,12 +1,34 @@
 package com.uph.tpsi.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.uph.tpsi.models.Service;
+import com.uph.tpsi.services.interfaces.ServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/api/invoices")
 @CrossOrigin
 public class ServiceController
 {
+        private final ServiceService serviceService;
+
+        @Autowired
+        public ServiceController ( ServiceService serviceService )
+        {
+                this.serviceService = serviceService;
+        }
+
+        @GetMapping
+        public List<Service> findAll ()
+        {
+                return serviceService.findAllServices();
+        }
+
+        @PostMapping
+        public Service create ( @RequestBody Service service )
+        {
+                return serviceService.create( service );
+        }
 }
